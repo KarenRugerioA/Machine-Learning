dafdb66291f9fc85c7592c2765fd5cee4502a446
Karen <kaar_rugerioo@outlook.com>
2022-09-13 18:16:05 -0500

    Implement a larger dataset and separated train and test sets

--- linearRegressionSci.py
+++ linearRegressionSci.py
@@ -15,26 +15,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from typing import Tuple
 from sklearn import linear_model
-
-def plot_results(x: np.ndarray, y: np.ndarray, y_prediction: np.ndarray, t_prediction: Tuple)->None:
-    """
-    Function to scatter the samples and the returned function
-    to be ploted
-    """
-    # t_prediction[0] is the number of claims
-    # t_prediction[1] is the total payment for all the claims in thousands
-    new_x = np.append(x, t_prediction[0])
-    new_y_prediction = np.append(y_prediction, t_prediction[1])
-
-    plt.scatter(x, y,  color='#626361')
-    plt.scatter(t_prediction[0], t_prediction[1], color='#050df7')
-    plt.annotate('Predicted value', xy=t_prediction, horizontalalignment='right', verticalalignment='top')
-    plt.plot(new_x, new_y_prediction, color='#F5451F')
-    plt.legend(['POINTS', 'PREDICTED VALUE','FITTED FUNCTION'])
-    plt.show()
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 
 if __name__ == '__main__':
+
     # Argument parsing
     parser = argparse.ArgumentParser(description='Linear Regression using Scikit learn', usage='python3 linearRegressionSci.py -p [# numbers of claims to predict total payment of]')
     parser.add_argument('-p', '--prediction', type=float, help='Numbers of claims to predict total payment of', required=True)
@@ -46,26 +31,56 @@ if __name__ == '__main__':
 
     # Read the dataset
     df = pd.read_csv('auto-insurance.csv')
-    # Calculate number of elements present on the data frame
-    elements = len(df)
-    x = df.values[:, :-1]
-    y = df.values[:, -1]
+
+    #Defining dependant and independant values
+    x = df.values[:, :-1] #Inependant variable (number of claims)
+    y = df.values[:, -1] #Dependant variable (cost in thousands Swedish Kronor)
+
+    """Separating test and train data, in this case, the 
+    test sample is going to be 20% of the dataset and the
+    train sample is going to be the 80% of the dataset
+    """
+    x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.20, random_state=1)
 
     # Apply the linear regression by Sklearn library
     regr = linear_model.LinearRegression()
+
     #Fit linear model
-    regr.fit(x, y)
+    regr.fit(x_train, y_train)
 
     #Predict using the linear model.
-    y_prediction = regr.predict(x)
+    y_prediction = regr.predict(x_train)
     print(f"Predicted values:\n {y_prediction}")
 
-    # Predict total payment for all the claims in thousands
-    predicted_at_value = regr.predict([[args.prediction]])
-    print(f"Prediction for {args.prediction} claims = {predicted_at_value[0]} thousands Swedish Kronors\n")
+    # Accuracy calculation of the train based on MSE
+    score = r2_score(y_train, y_prediction)
+    print(f"The score (based on train MSE) is: {score}\n")
 
-    # Create a tuple with the number of claims(x) and the predicted
-    # payment for the claims (y)
+    # Predict salary in USD for years of experience (INPUT)
+    predicted_at_value = regr.predict([[args.prediction]])
+    print(f"Prediction for {args.prediction} ensurance payment = {predicted_at_value[0]} Swedish Kronor\n")
+    
+    # Predict salary based on years of experience with test values
+    y_predTest = regr.predict(x_test)
+
+    # Accuracy calculation of the test based on MSE
+    score = r2_score(y_test, y_predTest)
+    print(f"The score (based on test MSE) is: {score}\n")
+
+    # Create a tuple with the years of experience(x) and the predicted salary (y)
     t_prediction = (args.prediction, predicted_at_value)
 
-    plot_results(x,y, y_prediction, t_prediction)
+    # Create a new array to include the x of the input given by the user and y predicted value
+    new_x = np.append(x_test, t_prediction[0])
+    new_y_prediction = np.append(y_predTest, t_prediction[1])
+
+    plt.scatter(x_train, y_train, color='green', alpha=0.4)
+    plt.scatter(x_test, y_predTest, color = 'black', alpha=0.7)
+    plt.scatter(t_prediction[0], t_prediction[1], color='#050df7')
+    plt.annotate('Predicted value', xy=t_prediction, horizontalalignment='right', verticalalignment='top')
+    plt.title("xtest-ypredtest")
+    plt.plot(new_x, new_y_prediction, color='#F5451F')
+    plt.legend(['TRAIN SET', 'TEST SET','PREDICTED VALUE', 'FITTED FUNCTION'])
+    plt.show()
+
+    #plot_results(Xtrain, ytrain, y_prediction, t_prediction)
