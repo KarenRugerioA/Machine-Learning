7280ec93546382a7734c82ba047934d13a315387
Karen <kaar_rugerioo@outlook.com>
2022-09-14 12:35:59 -0500

    Add comments to the code

--- linearRegressionSci.py
+++ linearRegressionSci.py
@@ -44,8 +44,6 @@ if __name__ == '__main__':
     """
     x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.20, random_state=1)
 
-    #separar train for validation(cross validation)
-
     # Apply the linear regression by Sklearn library
     regr = linear_model.LinearRegression()
 
@@ -54,20 +52,19 @@ if __name__ == '__main__':
 
     #Predict using the linear model.
     y_prediction = regr.predict(x_train)
-    print(f"Predicted values:\n {y_prediction}")
+    print(f"Predicted values:\n {y_prediction}\n")
 
-    # Accuracy calculation of the train based on MSE
+    # Accuracy calculation of predictions based on training set using r2
     score = r2_score(y_train, y_prediction)
     print(f"The r2 using train set is: {score}\n")
 
-    # Predict salary in USD for years of experience (INPUT)
+    # Predict payment in Swedish Kronor for a given number of claims (INPUT)
     predicted_at_value = regr.predict([[args.prediction]])
-    print(f"Prediction for {args.prediction} ensurance payment = {predicted_at_value[0]} Swedish Kronor\n")
     
-    # Predict salary based on years of experience with test values
+    # Predict payment in Swedish Kronor based on test values (number of claims)
     y_predTest = regr.predict(x_test)
 
-    # Accuracy calculation of the test based on MSE
+    # Accuracy calculation of predictions based on test set using r2
     score = r2_score(y_test, y_predTest)
     print(f"The r2 using test set is: {score}\n")
 
@@ -75,7 +72,7 @@ if __name__ == '__main__':
     score_CV = cross_val_score(regr, x, y, cv=2)
     print("Accuracy of %0.2f with a standard deviation of %0.2f" % (score_CV.mean(), score_CV.std()))
 
-    # Create a tuple with the years of experience(x) and the predicted salary (y)
+    # Create a tuple with the number of claims(x) and the Swedish kronor to pay (y)
     t_prediction = (args.prediction, predicted_at_value)
 
     # Create a new array to include the x of the input given by the user and y predicted value
@@ -85,18 +82,23 @@ if __name__ == '__main__':
     # A learning curve shows the validation and training score of an estimator for varying numbers of training samples
     train_sizes, train_scores, valid_scores = learning_curve(regr, x, y, cv=3)
 
+    # Calculating train's mean and train's standard deviation obtained when calculating the learning curve
     train_mean = np.mean(train_scores, axis=1)
     train_std = np.std(train_scores, axis=1)
-
+    
+    # Calculating test's means and test's standard deviation obtained when calculating the learning curve
     test_mean = np.mean(valid_scores, axis=1)
     test_std = np.std(valid_scores, axis=1)
 
+    # Applied Lasso regression for standardadization purposes
     lasso_model = linear_model.Lasso()
     lasso_model.fit(x_train, y_train)
     pred_yR = lasso_model.predict(x_test)
     score_CV = cross_val_score(regr, x, y, cv=3)
     print("Accuracy of %0.2f after applying Lasso with a standard deviation of %0.2f" % (score_CV.mean(), score_CV.std()))
+    print(f"Prediction for {args.prediction} ensurance payment = {predicted_at_value[0]} Swedish Kronor\n")
 
+    # Plotting the learning curve: Training score vs Cross-validation score.
     plt.subplot(1, 2, 1)
     plt.grid()
     plt.plot(train_sizes, train_mean, 'o-', color="red",  label="Training score")
@@ -107,6 +109,7 @@ if __name__ == '__main__':
     plt.xlabel("Training Set Size"), plt.ylabel("Accuracy Score")
     plt.legend(loc="best")
 
+    # Plotting the Linear Regression
     plt.subplot(1, 2, 2)
     plt.grid()
     plt.scatter(x_train, y_train, color='green', alpha=0.4)
@@ -117,5 +120,3 @@ if __name__ == '__main__':
     plt.plot(new_x, new_y_prediction, color='#F5451F')
     plt.legend(['TRAIN SET', 'TEST SET','PREDICTED VALUE', 'FITTED FUNCTION'])
     plt.show()
-
-    
